72eac234babfda863c222539ef0b62755d226c19
Samuel Bosawer <zam.bosawer@gmail.com>
2023-03-24 11:00:20 +0900

    Tampilkan data

--- README.md
+++ README.md
@@ -6,3 +6,16 @@ Belajar Database Manajemen Sistem
     ✅ Oracle https://www.oracle.com/id/
     ✅ Mongodb https://www.mongodb.com/
     ✅ Mariadb https://mariadb.org/ 
+
+### Query database
+#### Buat database 
+    CREATE DATABASE db_samuel
+### Hapus database 
+    DROP DATABASE db_samuel
+### Buat Table
+    CREATE TABLE `nama_table` 
+    ( `id_document` int(11) NOT NULL, `name_doc` varchar(255) NOT NULL, `document` varchar(255) NOT NULL );
+### Tambah Data
+    INSERT INTO `karyawan` (`id_karyawan`, `nama_karyawan`, `jk`, `email`, `alamat_jalan`, `alamat_distrik`) VALUES (NULL, 'Acho', 'L', 'acho@gmail.com', 'perumnas 3 Waena', 'Hedam'), (NULL, 'Marthen', 'L', 'marthen@gmail.com', 'Waena', 'Hedam');
+### Tampilkan data
+    SELECT * FROM `karyawan`
--- index.php
+++ index.php
@@ -1,3 +1,30 @@
+<?php
+    $serve = "localhost";
+    $username = "root";
+    $password = "";
+    $database = "db_samuel";
+
+    $koneksi = mysqli_connect($serve, $username, $password, $database);
+
+    $data = "SELECT * FROM `karyawan` ";
+    $hasil = mysqli_query($koneksi, $data);
+
+    // foreach(($hasil) as $tampilkan)
+    // {
+    //     var_dump($tampilkan);
+    //     // echo['nama_karyawan'];
+    // }
+
+    // while($row = mysqli_fetch_assoc($hasil))
+    // {
+    //     echo ($row['nama_karyawan']);
+    // }
+
+
+    mysqli_close($koneksi);
+?>
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -5,8 +32,33 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP DAY 03 - Belajar DBMS</title>
+    <script src="https://cdn.tailwindcss.com"></script>
 </head>
 <body>
-    
+    <div class="container mx-auto py-5">
+        <div class="row">
+            <div class="col">
+                <h2>Data Karyawan</h2>
+                <table class="border-collapse border border-slate-400 ">
+                    <tr>
+                        <th class="border border-slate-300">No</th>
+                        <th class="border border-slate-300">Nama</th>
+                        <th class="border border-slate-300">Email</th>
+                        <th class="border border-slate-300">Jenis</th>
+                        <th class="border border-slate-300">Alamat</th>
+                    </tr>
+                    <?php $no = 0; while($data_k = mysqli_fetch_assoc($hasil)) :  ?>
+                        <tr>
+                            <td class="border border-slate-300"> <?= ++$no?></td>
+                            <td class="border border-slate-300"><?php echo $data_k['nama_karyawan']?></td>
+                            <td class="border border-slate-300"><?= $data_k['email']?></td>
+                            <td class="border border-slate-300"><?= $data_k['jk']?></td>
+                            <td class="border border-slate-300"><?= $data_k['alamat_jalan'] .' '. $data_k['alamat_distrik']  ?></td>
+                        </tr>  
+                    <?php endwhile ?>    
+                </table>
+            </div>
+        </div>
+    </div>
 </body>
 </html>
